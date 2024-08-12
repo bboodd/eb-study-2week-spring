90c2c2a06b0fde943b9978e88429d926e2803eca
bboodd <gkrgus9@gmail.com>
2024-08-12 13:50:19 +0900

    db세팅

---
+++ README.md
@@ -0,0 +1,11 @@
+spring, mybatis로 게시판 만들기
+
+## Docker Compose 실행 - MySql
+``` 
+cd docker
+docker-compose up -d
+```
+
+database
+
+![img.png](erd.png)
--- src/main/java/com/study/week2/Week2Application.java
+++ src/main/java/com/study/week2/Week2Application.java
@@ -1,13 +1,27 @@
 package com.study.week2;
 
+import com.study.week2.connection.ConnectionTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Week2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Week2Application.class, args);
+    }
+
+    @GetMapping(value = "/")
+    public String doGetHelloWorld() {
+        try{
+            ConnectionTest t = new ConnectionTest();
+            return "connection: " + t.getConnection();
+        } catch (Exception e){
+            return "Connection err" + e;
+        }
     }
 
 }
