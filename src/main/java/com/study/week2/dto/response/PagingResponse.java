84ea9ebe23fb7439bf253156749621c3f279e6f8
bboodd <gkrgus9@gmail.com>
2024-08-19 16:59:14 +0900

    페이징 적용

---
+++ src/main/java/com/study/week2/dto/response/PagingResponse.java
@@ -0,0 +1,19 @@
+package com.study.week2.dto.response;
+
+import com.study.week2.vo.Pagination;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class PagingResponse<T> {
+
+    private List<T> list = new ArrayList<>();
+    private Pagination pagination;
+
+    public PagingResponse(List<T> list, Pagination pagination) {
+        this.list.addAll(list);
+        this.pagination = pagination;
+    }
+}
--- src/main/java/com/study/week2/vo/SearchVo.java
+++ src/main/java/com/study/week2/vo/SearchVo.java
@@ -17,6 +17,13 @@ public class SearchVo {
 
     private String keyword;
 
+
+    private int page;           //현재 페이지 번호
+    private int recordSize;     //페이지당 출력할 데이터 개수
+    private int pageSize;       //화면 하단 출력할 페이지 사이즈
+
+    private Pagination pagination;
+
     /**
      * dto를 vo로 변환
      * 화면에서 받아서 db로 넘길때
@@ -30,7 +37,15 @@ public class SearchVo {
                 .endDate(searchDto.getEndDate())
                 .categoryId(searchDto.getCategoryId())
                 .keyword(searchDto.getKeyword())
+                .page(searchDto.getPage())
+                .recordSize(searchDto.getRecordSize())
+                .pageSize(searchDto.getPageSize())
+                .pagination(searchDto.getPagination())
                 .build();
         return searchVo;
+    }
+
+    public int getOffset() {        //시작
+        return (page - 1) * recordSize;
     }
 }
